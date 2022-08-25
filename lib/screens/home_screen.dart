056ee55c6e98941223ef528edbbad5ff246d8e97
Williams W. Alvarez Zabala <williamswaz18@gmail.com>
2022-08-25 15:35:22 -0400

    how to navigate to routes by two ways

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:components_application/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,10 +6,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-            child: Text("HomeScreen")
-        )
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Componentes de flutter"),
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+              title: const Text("Nombre de la ruta"),
+              leading: const Icon(Icons.dangerous),
+              /* This is one way to push to another route
+              onTap: (){
+                final route = MaterialPageRoute(
+                    builder:(context) => const ListViewScreen()
+                );
+                Navigator.push(context, route);
+              },
+              And the following is a second way to push to another route*/
+              onTap: (){
+                Navigator.pushNamed(context, 'card');
+              },
+
+            ),
+            separatorBuilder: (_, __) => Divider(),
+            itemCount: 5)
     );
   }
 }
